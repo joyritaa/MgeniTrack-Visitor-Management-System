e5e77acaff0cb4b241ca5f7b9e5f44808e77392c
joyritaa <joyritanjoki061@gmail.com>
2026-05-20 14:15:11 +0300

    Fixed the password complexity requirements when creating a new user. added small text functionality in .css

--- ViewModels/UserCreateViewModel.cs
+++ ViewModels/UserCreateViewModel.cs
@@ -1,13 +1,31 @@
-﻿namespace MgeniTrack.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MgeniTrack.ViewModels
 {
     public class UserCreateViewModel
     {
         public required string Firstname { get; set; }
         public required string Secondname { get; set;}
         public required string Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+
+        [RegularExpression(
+            @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$",
+            ErrorMessage =
+            "Password must contain:\n" +
+            "- At least 8 characters\n" +
+            "- One uppercase letter\n" +
+            "- One lowercase letter\n" +
+            "- One number\n" +
+            "- One special character"
+        )]
         public required string Password { get; set; }
         public required string IdNumber { get; set; }
         public required string Email { get; set; }
+        
+        [Phone]
         public required string PhoneNumber { get; set; }
 
 
